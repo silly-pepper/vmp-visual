9c64c7607bed104bc2c1394f1d13e236e0cd91d5
lisache121 <lisache121@gmail.com>
2024-10-17 17:52:34 +0300

    init commit

---
+++ src/components/Visual.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="unity-container">
+    <iframe src="/Build/Build.html" frameborder="0" style="width: 1200px; height: 600px;"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UnityView'
+}
+</script>
+
+<style scoped>
+#unity-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
+</style>
